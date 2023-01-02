db72f08ec564ccba992e6c3a5149ee8005ca60d4
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-01-01 20:50:00 -0800

    Create basic site structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <h1>Testing....</h1>
+      <div className="page-wrap">
+        <header className="page-header">
+          <h1>CV Builder</h1>
+        </header>
+        <main className="page-main">
+          <p>Main....</p>
+        </main>
+        <footer className="page-footer">
+          <small>Ray Allen Datuin 2023</small>
+        </footer>
+      </div>
     );
   }
 }
