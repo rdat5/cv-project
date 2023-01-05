e7f21edff9507761c405ad9c0bee05542ef59b16
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-01-04 22:18:43 -0800

    Add data input component

---
+++ src/components/DataInputForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import GeneralInfoInput from './GeneralInfoInput';
+
+class DataInputForm extends Component {
+    render() {
+        return (
+            <p>DATA INPUT FORM</p>
+        );
+    }
+}
+
+export default DataInputForm;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import DataInputForm from './DataInputForm';
 
 class Main extends Component {
     render() {
         return (
             <main className='page-main'>
-                <p>MAIN...</p>
+                <DataInputForm/>
             </main>
         );
     }
