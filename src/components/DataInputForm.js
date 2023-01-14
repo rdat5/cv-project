0020b7640e9c3879874cc33e29c2d7c19f193f4a
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-01-13 23:51:51 -0800

    Combine input components into one form for simplicity

--- src/components/DataInputForm.js
+++ src/components/DataInputForm.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-import GeneralInfoInput from './GeneralInfoInput';
-import EduInputForm from './EduInputForm';
-import PracInputForm from './PracInputForm';
 
 class DataInputForm extends Component {
     render() {
         return (
             <div className='data-input-form'>
-                <GeneralInfoInput/>
-                <EduInputForm/>
-                <PracInputForm/>
+                <form>
+                    <fieldset>
+                        <legend>General Information</legend>
+                        <label htmlFor="fullName">Name:</label>
+                        <input type="text" id="fullName" name="fullName"></input>
+                        <br></br>
+                        <br></br>
+                        <label htmlFor="email">Email:</label>
+                        <input type="text" id="email" name="email"></input>
+                        <br></br>
+                        <br></br>
+                        <label htmlFor="phone">Phone:</label>
+                        <input type="text" id="phone" name="phone"></input>
+                        <br></br>
+                        <br></br>
+                        <button>Submit</button>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Educational Experience</legend>
+                        <button>+ Add Entry</button>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Practical Experience</legend>
+                        <button>+ Add Entry</button>
+                    </fieldset>
+                </form>
             </div>
         );
     }
