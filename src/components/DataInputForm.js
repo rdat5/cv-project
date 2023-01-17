61e33fc800e0765c7180f7fdaeb394d9bbecb278
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-01-17 00:00:05 -0800

    Add form for entering practical experience

--- src/components/DataInputForm.js
+++ src/components/DataInputForm.js
@@ -100,7 +100,45 @@ class DataInputForm extends Component {
                     </fieldset>
                     <fieldset>
                         <legend>Practical Experience</legend>
-                        <button>+ Add Entry</button>
+                        <fieldset>
+                            <legend>Practical Experience Entry</legend>
+                            <label htmlFor="companyNameInput">Company Name:</label>
+                            <input
+                                type="text"
+                                id="companyNameInput"
+                                name="companyNameInput"/>
+                            <br></br>
+                            <br></br>
+                            <label htmlFor="positionInput">Position Title:</label>
+                            <input
+                                type="text"
+                                id="positionInput"
+                                name="positionInput"/>
+                            <br></br>
+                            <br></br>
+                            <label htmlFor="mainTaskInput">Main Tasks:</label>
+                            <input
+                                type="text"
+                                id="mainTaskInput"
+                                name="mainTaskInput"/>
+                            <br></br>
+                            <br></br>
+                            <label htmlFor="workedFromInput">Worked from:</label>
+                            <input
+                                type="text"
+                                id="workedFromInput"
+                                name="workedFromInput"/>
+                            <br></br>
+                            <br></br>
+                            <label htmlFor="workedToInput">Worked to:</label>
+                            <input
+                                type="text"
+                                id="workedToInput"
+                                name="workedToInput"/>
+                            <br></br>
+                            <br></br>
+                            <button>+ Add Entry</button>
+                        </fieldset>
                     </fieldset>
                     <button onClick={this.handleSubmit}>Submit</button>
                 </form>
