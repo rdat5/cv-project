1ebbf3b38f39ee4db99a63aa417fe27b009e67eb
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-01-24 22:44:09 -0800

    Seperate educational experience entry as its own component

--- src/components/EduExpInput.js
+++ src/components/EduExpInput.js
@@ -1,36 +1,10 @@
 import React, { Component } from 'react';
+import EduEntry from './EduEntry';
 
 class EduExpInput extends Component {
     render() {
         return(
-            <fieldset>
-                <legend>Educational Experience</legend>
-                <fieldset>
-                    <legend>Education Entry</legend>
-                    <label htmlFor="schoolNameInput">School Name:</label>
-                    <input
-                        type="text"
-                        id="schoolNameInput"
-                        name="schoolNameInput"/>
-                    <br></br>
-                    <br></br>
-                    <label htmlFor="studyInput">Study:</label>
-                    <input
-                        type="text"
-                        id="studyInput"
-                        name="studyInput"/>
-                    <br></br>
-                    <br></br>
-                    <label htmlFor="studyDateInput">Date of Study:</label>
-                    <input
-                        type="text"
-                        id="studyDateInput"
-                        name="studyDateInput"/>
-                    <br></br>
-                    <br></br>
-                    <button>+ Add Entry</button>
-                </fieldset>
-            </fieldset>
+            <EduEntry/>
         );
     }
 }
