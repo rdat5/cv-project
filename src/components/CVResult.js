e2708c658816d0203ef4fe206ed2a84567df02de
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-01-24 22:28:34 -0800

    Add section in CV result for list of education entries

--- src/components/CVResult.js
+++ src/components/CVResult.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 class CVResult extends Component {
     render() {
         const userData = this.props.userData;
+        const eduList = this.props.userData.userEdu.map((entry) => 
+            <li key={entry.id}>{entry.schoolNameInput} - {entry.studyInput} - {entry.studyDateInput}</li>
+        )
 
         return (
             <div className='cv-result'>
@@ -10,6 +13,8 @@ class CVResult extends Component {
                 <p>Name: {userData.nameInput}</p>
                 <p>Email: {userData.emailInput}</p>
                 <p>Phone: {userData.phoneInput}</p>
+                <p>Education:</p>
+                <ul>{eduList}</ul>
             </div>
         );
     }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DataInputForm from './DataInputForm';
 import CVResult from './CVResult';
+import uniqid from 'uniqid';
 
 class Main extends Component {
     constructor(props) {
@@ -10,7 +11,20 @@ class Main extends Component {
             nameInput: '',
             emailInput: '',
             phoneInput: '',
-            userEdu: [],
+            userEdu: [
+                {
+                    id: uniqid(),
+                    schoolNameInput: 'test name',
+                    studyInput: 'test study',
+                    studyDateInput: 'test date'
+                },
+                {
+                    id: uniqid(),
+                    schoolNameInput: 'test name 2',
+                    studyInput: 'test study 2',
+                    studyDateInput: 'test date 2'
+                }
+            ],
             userPrac: []
         };
 
