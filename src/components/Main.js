fc71ff6f9a8a14b9b2f6393c4f5e75ee76fe6d57
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-02-04 08:43:10 -0800

    Fix typo

--- src/components/Main.js
+++ src/components/Main.js
@@ -43,7 +43,7 @@ class Main extends Component {
 
         return (
             <main className='page-main'>
-                {currentScene};
+                {currentScene}
             </main>
         );
     }
