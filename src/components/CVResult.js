03c7fea5ea5f10dff3ae674102b98344c5db975f
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-02-03 23:18:27 -0800

    Add feature of omitting practical experiece section if there are no available entries

--- src/components/CVResult.js
+++ src/components/CVResult.js
@@ -28,15 +28,21 @@ class CVResult extends Component {
             </div>
         }
 
+        let ExperienceSection;
+
+        if (userData.userPrac.length > 0) {
+            ExperienceSection = <div>
+                <h3>Experience</h3>
+                <ul>{pracList}</ul>
+            </div>
+        }
+
         return (
             <div className='cv-result'>
                 <h2>{userData.nameInput} | Curriculum Vitae</h2>
                 <p>{userData.phoneInput} | {userData.emailInput}</p>
-                {/* <h3>Education</h3>
-                <ul>{eduList}</ul> */}
                 {EducationSection}
-                <h3>Experience</h3>
-                <ul>{pracList}</ul>
+                {ExperienceSection}
             </div>
         );
     }
